0163563e3a2ee4496a22a7dcc8f1ea7f1c716145
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-11 17:02:48 +0700

    Shell: create shell to check the required environment

---
+++ script/doctor.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+ERR_COUNT=0
+
+main() {
+  # Check Programming Runtimes
+  check_cmd "go"
+  check_cmd "migrate" "golang-migrate"
+  check_optional_cmd "brew"
+  check_optional_cmd "docker"
+
+  echo
+
+  if [[ ${ERR_COUNT} -gt 0 ]]; then
+    echo "Error Count: ${ERR_COUNT}"
+    exit 1
+  fi
+
+  echo "OK"
+}
+
+check_cmd() {
+  # Get arguments
+  CMD_NAME=$1
+
+  if ! [[ -z "$2" ]]; then
+    CMD_NAME=$2
+  fi
+
+  echo -e "Checking command ${CMD_NAME}: \c"
+
+  if ! [[ -x "$(command -v "$1")" ]]; then
+    ERR_COUNT=$((ERR_COUNT + 1))
+    echo "NOT FOUND"
+
+  else
+    echo "OK"
+  fi
+}
+
+check_optional_cmd() {
+  # Get arguments
+  CMD_NAME=$1
+
+  echo -e "Checking optional command ${CMD_NAME}: \c"
+
+  if ! [[ -x "$(command -v "${CMD_NAME}")" ]]; then
+    echo "NOT FOUND"
+
+  else
+    echo "OK"
+  fi
+}
+
+check_script() {
+  # Get arguments
+  SCRIPT_FILE=$1
+
+  echo -e "Checking script ${SCRIPT_FILE}: \c"
+
+  if ! [[ -f ${SCRIPT_FILE} && -x ${SCRIPT_FILE} ]]; then
+    ERR_COUNT=$((ERR_COUNT + 1))
+    echo "NON EXECUTABLE"
+  else
+    echo "OK"
+  fi
+}
+
+main
