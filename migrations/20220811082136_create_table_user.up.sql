16b21e58bef02628e814e9ffffd0ce82f1cee634
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-11 17:04:58 +0700

    Migration: create table user migration file

---
+++ migrations/20220811082136_create_table_user.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(255) UNIQUE PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL 
+)
+
