2db344588da9a0a01f53914f2476c42105d92148
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-12 13:08:57 +0700

    Utils: solve test case for bcrypt

--- app/utils/bcrypt.go
+++ app/utils/bcrypt.go
@@ -1,9 +1,23 @@
 package utils
 
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 func HashPassword(password string) string {
-	return ""
+	pw := []byte(password)
+	result, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(result)
 }
 
 func ComparePassword(hashPassword string, password string) error {
-	return nil
+	pw := []byte(password)
+	hw := []byte(hashPassword)
+	err := bcrypt.CompareHashAndPassword(hw, pw)
+	return err
 }
