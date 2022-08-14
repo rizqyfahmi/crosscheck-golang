662e53ed68f45caf365503fbafbbeac5c173086f
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-14 10:38:53 +0700

    Utils: simplifying test case

--- tests/utils/bcrypt_test.go
+++ tests/utils/bcrypt_test.go
@@ -1,4 +1,4 @@
-package utils_test
+package utils
 
 import (
 	. "github.com/onsi/ginkgo/v2"
@@ -8,65 +8,65 @@ import (
 	"crosscheck-golang/app/utils"
 )
 
-var _ = Describe("Hash password", func() {
-	Context("When the result is compared with different password", func() {
-		It("returns failure", func() {
-			hashPassword := utils.HashPassword("HelloPassword")
-			err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte("OllaPassword"))
+var _ = Describe("Bcrypt", func() {
+	Describe("Hash Password", func() {
+		Context("When the result is compared with different password", func() {
+			It("returns failure", func() {
+				hashPassword := utils.HashPassword("HelloPassword")
+				err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte("OllaPassword"))
 
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("crypto/bcrypt: hashedPassword is not the hash of the given password"))
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("crypto/bcrypt: hashedPassword is not the hash of the given password"))
+			})
+		})
+
+		Context("When the result is compared with similar password", func() {
+			It("returns success", func() {
+				hashPassword := utils.HashPassword("HelloPassword")
+				err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte("HelloPassword"))
+
+				Expect(err).Should(Succeed())
+			})
+		})
+
+		Context("When the password parameter is already encrypted", func() {
+			It("returns failure", func() {
+				hashPassword := utils.HashPassword("$2a$12$kSmt/kAF0Yf0egtWzvWQR.XOcpy0QkG7qe5BWKfCua.nUw3fqguSS")
+				err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte("OllaPassword"))
+
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("crypto/bcrypt: hashedPassword is not the hash of the given password"))
+			})
 		})
 	})
 
-	Context("When the result is compared with similar password", func() {
-		It("returns success", func() {
-			hashPassword := utils.HashPassword("HelloPassword")
-			err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte("HelloPassword"))
+	Describe("Compare password", func() {
+		var result []byte
+
+		BeforeEach(func() {
+			pw := []byte("HelloPassword")
+			tempResult, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
+
+			result = tempResult
 
 			Expect(err).Should(Succeed())
 		})
-	})
 
-	Context("When the password parameter is already encrypted", func() {
-		It("returns failure", func() {
-			hashPassword := utils.HashPassword("$2a$12$kSmt/kAF0Yf0egtWzvWQR.XOcpy0QkG7qe5BWKfCua.nUw3fqguSS")
-			err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte("OllaPassword"))
+		Context("When the password parameters are not equal", func() {
+			It("returns failure", func() {
+				err := utils.ComparePassword(string(result), "OllaPassword")
 
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("crypto/bcrypt: hashedPassword is not the hash of the given password"))
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("crypto/bcrypt: hashedPassword is not the hash of the given password"))
+			})
+		})
+
+		Context("When the password parameters are equal", func() {
+			It("returns success", func() {
+				err := utils.ComparePassword(string(result), "HelloPassword")
+
+				Expect(err).Should(Succeed())
+			})
 		})
 	})
-
-})
-
-var _ = Describe("Compare password", func() {
-	var result []byte
-
-	BeforeEach(func() {
-		pw := []byte("HelloPassword")
-		tempResult, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
-
-		result = tempResult
-
-		Expect(err).Should(Succeed())
-	})
-
-	Context("When the password parameters are not equal", func() {
-		It("returns failure", func() {
-			err := utils.ComparePassword(string(result), "OllaPassword")
-
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("crypto/bcrypt: hashedPassword is not the hash of the given password"))
-		})
-	})
-
-	Context("When the password parameters are equal", func() {
-		It("returns success", func() {
-			err := utils.ComparePassword(string(result), "HelloPassword")
-
-			Expect(err).Should(Succeed())
-		})
-	})
-
 })
--- tests/utils/jwt_test.go
+++ tests/utils/jwt_test.go
@@ -1,7 +1,6 @@
-package utils_test
+package utils
 
 import (
-	"log"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -10,125 +9,126 @@ import (
 	"crosscheck-golang/app/utils"
 )
 
-var _ = Describe("Generate Token", func() {
-	Context("When SecretKey parameter on initial is empty", func() {
-		It("returns failed", func() {
-			jwt := utils.New("", 10*time.Minute)
-			result, err := jwt.GenerateToken("UserID")
+var _ = Describe("JWT", func() {
+	Describe("Generate Token", func() {
+		Context("When SecretKey parameter on initial is empty", func() {
+			It("returns failed", func() {
+				jwt := utils.New("", 10*time.Minute)
+				result, err := jwt.GenerateToken("UserID")
 
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("SecretKey is required"))
-			Expect(result).Should(BeNil())
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("SecretKey is required"))
+				Expect(result).Should(BeNil())
+			})
+		})
+
+		Context("When ExpiresAt parameter on initial is empty", func() {
+			It("returns failed", func() {
+				jwt := utils.New("SecretKey", 0)
+				result, err := jwt.GenerateToken("UserID")
+
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("ExpiresAt must be greater than 0"))
+				Expect(result).Should(BeNil())
+			})
+		})
+
+		Context("When UserID parameter on GenerateToken is empty", func() {
+			It("returns failed", func() {
+				jwt := utils.New("SecretKey", 10*time.Minute)
+				result, err := jwt.GenerateToken("")
+
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("UserID is required"))
+				Expect(result).Should(BeNil())
+			})
+		})
+
+		Context("When all required parameters is not empty", func() {
+			It("returns success", func() {
+				jwt := utils.New("SecretKey", 10*time.Minute)
+				result, err := jwt.GenerateToken("UserID")
+
+				Expect(err).Should(Succeed())
+				Expect(result).ShouldNot(BeNil())
+			})
 		})
 	})
 
-	Context("When ExpiresAt parameter on initial is empty", func() {
-		It("returns failed", func() {
-			jwt := utils.New("SecretKey", 0)
-			result, err := jwt.GenerateToken("UserID")
+	Describe("Validate Token", func() {
+		Context("When SecretKey parameter on initial is empty", func() {
+			It("returns failed", func() {
+				jwt := utils.New("", 10*time.Minute)
+				result, err := jwt.ValidateToken("Token")
 
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("ExpiresAt must be greater than 0"))
-			Expect(result).Should(BeNil())
-		})
-	})
-
-	Context("When UserID parameter on GenerateToken is empty", func() {
-		It("returns failed", func() {
-			jwt := utils.New("SecretKey", 10*time.Minute)
-			result, err := jwt.GenerateToken("")
-
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("UserID is required"))
-			Expect(result).Should(BeNil())
-		})
-	})
-
-	Context("When all required parameters is not empty", func() {
-		It("returns success", func() {
-			jwt := utils.New("SecretKey", 10*time.Minute)
-			result, err := jwt.GenerateToken("UserID")
-
-			Expect(err).Should(Succeed())
-			Expect(result).ShouldNot(BeNil())
-		})
-	})
-})
-
-var _ = Describe("Validate Token", func() {
-	Context("When SecretKey parameter on initial is empty", func() {
-		It("returns failed", func() {
-			jwt := utils.New("", 10*time.Minute)
-			result, err := jwt.ValidateToken("Token")
-
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("SecretKey is required"))
-			Expect(result).Should(BeNil())
-		})
-	})
-
-	Context("When ExpiresAt parameter on initial is empty", func() {
-		It("returns failed", func() {
-			jwt := utils.New("SecretKey", 0)
-			result, err := jwt.ValidateToken("Token")
-
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("ExpiresAt must be greater than 0"))
-			Expect(result).Should(BeNil())
-		})
-	})
-
-	Context("When token parameter on ValidateToken is empty", func() {
-		It("returns failed", func() {
-			jwt := utils.New("SecretKey", 10*time.Minute)
-			result, err := jwt.ValidateToken("")
-
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("token is required"))
-			Expect(result).Should(BeNil())
-		})
-	})
-
-	Context("When token parameter on ValidateToken is empty", func() {
-		It("returns failed", func() {
-			jwt := utils.New("SecretKey", 10*time.Minute)
-			result, err := jwt.ValidateToken("")
-
-			Expect(err).Should(HaveOccurred())
-			Expect(err).Should(MatchError("token is required"))
-			Expect(result).Should(BeNil())
-		})
-	})
-
-	Context("When value of token parameter is invalid", func() {
-		It("returns failed", func() {
-			jwt := utils.New("SecretKey", 10*time.Minute)
-			result, err := jwt.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
-
-			log.Println(result)
-			Expect(err).Should(HaveOccurred())
-			Expect(result).ShouldNot(BeNil())
-		})
-	})
-
-	Context("When all required parameters is not empty", func() {
-		var token string
-		var jwt *utils.JwtUtil
-
-		BeforeEach(func() {
-			jwt = utils.New("SecretKey", 30*time.Minute)
-			tempToken, err := jwt.GenerateToken("UserID")
-
-			token = *tempToken
-
-			Expect(err).Should(Succeed())
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("SecretKey is required"))
+				Expect(result).Should(BeNil())
+			})
 		})
 
-		It("returns success", func() {
-			result, err := jwt.ValidateToken(token)
+		Context("When ExpiresAt parameter on initial is empty", func() {
+			It("returns failed", func() {
+				jwt := utils.New("SecretKey", 0)
+				result, err := jwt.ValidateToken("Token")
 
-			Expect(err).Should(Succeed())
-			Expect(result).ShouldNot(BeNil())
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("ExpiresAt must be greater than 0"))
+				Expect(result).Should(BeNil())
+			})
+		})
+
+		Context("When token parameter on ValidateToken is empty", func() {
+			It("returns failed", func() {
+				jwt := utils.New("SecretKey", 10*time.Minute)
+				result, err := jwt.ValidateToken("")
+
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("token is required"))
+				Expect(result).Should(BeNil())
+			})
+		})
+
+		Context("When token parameter on ValidateToken is empty", func() {
+			It("returns failed", func() {
+				jwt := utils.New("SecretKey", 10*time.Minute)
+				result, err := jwt.ValidateToken("")
+
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(MatchError("token is required"))
+				Expect(result).Should(BeNil())
+			})
+		})
+
+		Context("When value of token parameter is invalid", func() {
+			It("returns failed", func() {
+				jwt := utils.New("SecretKey", 10*time.Minute)
+				result, err := jwt.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+
+				Expect(err).Should(HaveOccurred())
+				Expect(result).ShouldNot(BeNil())
+			})
+		})
+
+		Context("When all required parameters is not empty", func() {
+			var token string
+			var jwt *utils.JwtUtil
+
+			BeforeEach(func() {
+				jwt = utils.New("SecretKey", 30*time.Minute)
+				tempToken, err := jwt.GenerateToken("UserID")
+
+				token = *tempToken
+
+				Expect(err).Should(Succeed())
+			})
+
+			It("returns success", func() {
+				result, err := jwt.ValidateToken(token)
+
+				Expect(err).Should(Succeed())
+				Expect(result).ShouldNot(BeNil())
+			})
 		})
 	})
 })
