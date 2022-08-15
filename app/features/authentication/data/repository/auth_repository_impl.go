c2adb9e4482049c39d8cf8c0fc711446d0491319
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-16 00:22:03 +0700

    Authentication: solve test case for registration repository

--- app/features/authentication/data/repository/auth_repository_impl.go
+++ app/features/authentication/data/repository/auth_repository_impl.go
@@ -2,6 +2,7 @@ package authentication_repository
 
 import (
 	"crosscheck-golang/app/exception"
+	"crosscheck-golang/app/features/authentication/data/model"
 	"crosscheck-golang/app/features/authentication/data/param"
 	authenticationPersistent "crosscheck-golang/app/features/authentication/data/source"
 	"crosscheck-golang/app/features/authentication/domain/entity"
@@ -22,5 +23,28 @@ func New(authLocalData authenticationPersistent.AuthPersistentDataSource, clock 
 }
 
 func (repo *AuthRepositoryImpl) Registration(param param.RegistrationParam) (*entity.UserEntity, *exception.Exception) {
-	return nil, nil
+
+	userModel := model.UserModel{
+		Id:        repo.clock.Now().Format("20060102150405"),
+		Name:      param.Name,
+		Email:     param.Email,
+		Password:  param.Password,
+		CreatedAt: repo.clock.Now(),
+		UpdatedAt: repo.clock.Now(),
+	}
+
+	if err := repo.authLocalData.Insert(&userModel); err != nil {
+		return nil, &exception.Exception{
+			Message: exception.ErrorDatabase,
+			Causes:  err.Error(),
+		}
+	}
+
+	userEntity := entity.UserEntity{
+		Id:    userModel.Id,
+		Name:  userModel.Name,
+		Email: userModel.Email,
+	}
+
+	return &userEntity, nil
 }
