9ddf7d49db39df4a1a77a5b2c9efe9ab49b72ec2
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-15 20:23:49 +0700

    Authentication: simplifying test case for registration usecase

--- app/features/authentication/domain/usecase/registration_usecase.go
+++ app/features/authentication/domain/usecase/registration_usecase.go
@@ -1,10 +1,11 @@
-package usecase
+package registration_usecase
 
 import (
 	"crosscheck-golang/app/exception"
 	"crosscheck-golang/app/features/authentication/data/param"
 	"crosscheck-golang/app/features/authentication/domain/entity"
 	"crosscheck-golang/app/features/authentication/domain/repository"
+	"crosscheck-golang/app/utils/hash"
 	jwtUtil "crosscheck-golang/app/utils/jwt"
 )
 
@@ -12,17 +13,31 @@ type RegistrationUsecase struct {
 	authRepository   repository.AuthRepository
 	accessTokenUtil  jwtUtil.JwtUtil
 	refreshTokenUtil jwtUtil.JwtUtil
+	hash             hash.Hash
 }
 
-func New(authRepository repository.AuthRepository, accessTokenUtil jwtUtil.JwtUtil, refreshTokenUtil jwtUtil.JwtUtil) *RegistrationUsecase {
+func New(authRepository repository.AuthRepository, accessTokenUtil jwtUtil.JwtUtil, refreshTokenUtil jwtUtil.JwtUtil, hash hash.Hash) *RegistrationUsecase {
 	return &RegistrationUsecase{
 		authRepository,
 		accessTokenUtil,
 		refreshTokenUtil,
+		hash,
 	}
 }
 
 func (usecase *RegistrationUsecase) Call(param param.RegistrationParam) (*entity.AuthEntity, *exception.Exception) {
+
+	hashedPassword, err := usecase.hash.HashPassword(param.Password)
+
+	if err != nil {
+		return nil, &exception.Exception{
+			Message: exception.ErrorEncryption,
+			Causes:  err.Error(),
+		}
+	}
+
+	param.Password = *hashedPassword
+
 	user, errException := usecase.authRepository.Registration(param)
 	if errException != nil {
 		return nil, errException
