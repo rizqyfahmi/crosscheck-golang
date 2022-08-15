78bc6f35fb7db49b5df3a04b44d33d69fd2ddcc5
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-15 16:35:29 +0700

    Utils: change bcrypt.go becomes a helper class for golang.org/x/crypto/bcrypt

--- app/utils/bcrypt/bcrypt.go
+++ app/utils/bcrypt/bcrypt.go
@@ -4,20 +4,22 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) (*string, error) {
-	pw := []byte(password)
-	encrypted, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
-	if err != nil {
-		return nil, err
-	}
-
-	result := string(encrypted)
-	return &result, nil
+// this file is used for being a helper to create selector of golang.org/x/crypto/bcrypt where it isn't provided as default
+type Bcrypt interface {
+	GenerateFromPassword(password []byte, cost int) ([]byte, error)
+	CompareHashAndPassword(hashedPassword []byte, password []byte) error
 }
 
-func ComparePassword(hashPassword string, password string) error {
-	pw := []byte(password)
-	hw := []byte(hashPassword)
-	err := bcrypt.CompareHashAndPassword(hw, pw)
-	return err
+type BcryptImpl struct{}
+
+func New() Bcrypt {
+	return &BcryptImpl{}
+}
+
+func (s *BcryptImpl) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
+	return bcrypt.GenerateFromPassword(password, cost)
+}
+
+func (s *BcryptImpl) CompareHashAndPassword(hashedPassword []byte, password []byte) error {
+	return bcrypt.CompareHashAndPassword(hashedPassword, password)
 }
