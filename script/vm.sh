85024728d45353598cc3edb98cc911aaab431d71
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-20 22:35:51 +0700

    CI/CD: Networking

--- script/vm.sh
+++ script/vm.sh
@@ -1,14 +1,14 @@
 main() {
+
+    command docker network create internal-container-network || command docker network ls
+
     if [[ $(command docker images -q postgres) == '' ]]; then 
         echo "Pulling image..."
         command docker pull postgres:11-alpine
     fi
 
-    if [[ $(command docker ps -aq -f name=app-database) == "" ]]; then 
-        echo "Running postgres container..."
-        command docker run --name app-database -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v pwd:/var/lib/postgresql/data -d postgres:11-alpine
-    fi
-    
+    command docker network create internal-container-network || command docker network ls
+    command docker run --name app-database -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres --network internal-container-network -p 5432:5432 -v pwd:/var/lib/postgresql/data -d postgres:11-alpine || command docker ps -a
 }
 
 main
