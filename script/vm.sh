cd700da16b604f2f7c4753373c48d3d10219181a
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-20 23:20:56 +0700

    CI/CD: Remove project

--- script/vm.sh
+++ script/vm.sh
@@ -9,6 +9,7 @@ main() {
 
     command docker network create internal-container-network || command docker network ls
     command docker run --name app-database -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres --network internal-container-network -p 5432:5432 -v pwd:/var/lib/postgresql/data -d postgres:11-alpine || command docker ps -a
+    command docker exec app-database psql -U postgres -c "SELECT 1 FROM pg_database WHERE datname = 'crosscheck'" | grep -q 1 || docker exec app-database psql -U postgres -c "CREATE DATABASE crosscheck"
 }
 
 main
