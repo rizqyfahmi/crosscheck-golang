6df48b4a96b18178d4918549ebecb4b8e829043e
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-20 20:22:40 +0700

    CI/CD: Run vm.sh file

--- script/vm.sh
+++ script/vm.sh
@@ -4,11 +4,14 @@ main() {
         command docker pull postgres:11-alpine
     fi
 
-    if [[ $(docker ps -aq -f name=app-database) != "" ]]; then 
+    if [[ $(command docker ps -aq -f name=app-database) == "" ]]; then 
         echo "Running postgres container..."
-        command docker run --name app-database -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /data:/var/lib/postgresql/data -d postgres
+        command docker run --name app-database -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v pwd:/var/lib/postgresql/data -d postgres:11-alpine
     fi
 
+
+    command docker exec -it app-database psql -U postgres -c "SELECT 1 FROM pg_database WHERE datname = 'crosscheck'" | grep -q 1 || docker exec -it app-database psql -U postgres -c "CREATE DATABASE crosscheck"
+    
 }
 
 main
