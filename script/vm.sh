943a3307a682c8cfd0737bd0f00acbbb5be01981
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-20 20:36:30 +0700

    CI/CD: Remove create database command

--- script/vm.sh
+++ script/vm.sh
@@ -8,9 +8,6 @@ main() {
         echo "Running postgres container..."
         command docker run --name app-database -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v pwd:/var/lib/postgresql/data -d postgres:11-alpine
     fi
-
-
-    command docker exec -it app-database psql -U postgres -c "SELECT 1 FROM pg_database WHERE datname = 'crosscheck'" | grep -q 1 || docker exec -it app-database psql -U postgres -c "CREATE DATABASE crosscheck"
     
 }
 
