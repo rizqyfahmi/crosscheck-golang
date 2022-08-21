4318c3b3071d87577ee38200481651a629a7b27f
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-08-22 01:23:15 +0700

    Authentication: solve test case for login controller

--- app/features/authentication/presentation/http/controller/auth_controller.go
+++ app/features/authentication/presentation/http/controller/auth_controller.go
@@ -3,7 +3,9 @@ package authcontroller
 import (
 	"crosscheck-golang/app/exception"
 	"crosscheck-golang/app/features/authentication/data/param"
+	loginusecase "crosscheck-golang/app/features/authentication/domain/usecase/login"
 	registrationusecase "crosscheck-golang/app/features/authentication/domain/usecase/registration"
+	"crosscheck-golang/app/utils/response"
 	"log"
 	"net/http"
 
@@ -12,11 +14,13 @@ import (
 
 type AuthController struct {
 	registraion registrationusecase.RegistrationUsecase
+	login       loginusecase.LoginUsecase
 }
 
-func New(registraion registrationusecase.RegistrationUsecase) *AuthController {
+func New(registraion registrationusecase.RegistrationUsecase, login loginusecase.LoginUsecase) *AuthController {
 	return &AuthController{
 		registraion: registraion,
+		login:       login,
 	}
 }
 
@@ -43,7 +47,44 @@ func (controller *AuthController) Registration(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, exception.BadRequest)
 	}
 
-	log.Println("Completed!")
+	log.Println("\nRegistration controller: Completed!")
 	return c.JSON(http.StatusOK, authEntity)
+
+}
+
+func (controller *AuthController) Login(c echo.Context) error {
+	param := new(param.LoginParam)
+
+	log.Printf("\nLogin controller: binding request parameters")
+	if err := c.Bind(param); err != nil {
+		log.Printf("\nLogin controller: error binding! -> %+v", err)
+		return c.JSON(http.StatusInternalServerError, response.Response{
+			Message: exception.InternalServerError,
+		})
+	}
+
+	log.Printf("\nLogin controller: validating request parameters")
+	if err := c.Validate(param); err != nil {
+		log.Printf("\nLogin controller: error validation! -> %+v", err)
+		return c.JSON(http.StatusBadRequest, response.Response{
+			Message: exception.BadRequest,
+		})
+	}
+
+	log.Printf("\nLogin controller: executing registration usecase with params: %+v", param)
+	authEntity, err := controller.login.Call(*param)
+
+	if err != nil {
+		log.Printf("\nLogin controller: error usecase! -> %+v", err)
+		return c.JSON(http.StatusBadRequest, response.Response{
+			Message: exception.BadRequest,
+		})
+	}
+
+	log.Println("\nLogin controller: Completed!")
+	return c.JSON(http.StatusOK, response.Response{
+		Message: response.RequestSuccess,
+		Data:    authEntity,
+	})
 
 }
